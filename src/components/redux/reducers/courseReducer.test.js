0ad40ff6e69333c4b6bac9385122f826775e23af
Arjit <arjit.sharma@gmail.com>
2021-01-30 22:04:11 +0000

    One more Unit testcase for redux - Reducers

--- src/components/redux/reducers/courseReducer.test.js
+++ src/components/redux/reducers/courseReducer.test.js
@@ -1,7 +1,7 @@
 import courseReducer from "./courseReducer";
 import * as courseActions from "../actions/courseActions";
 
-it("Test Reducers", () => {
+it("Test Reducers - Create Course", () => {
   // Arrange
   const initialState = [
     {
@@ -29,4 +29,35 @@ it("Test Reducers", () => {
   expect(resultNewState.length).toEqual(3);
   expect(resultNewState[0].title).toEqual("A");
   expect(resultNewState[2].title).toEqual("C");
+});
+
+it("Test Reducers - Create Course", () => {
+  // Arrange
+  const initialState = [
+    {
+      id: 1,
+      title: "A",
+    },
+    {
+      id: 2,
+      title: "B",
+    },
+    {
+      id: 3,
+      title: "C",
+    },
+  ];
+
+  const courseUpdated = { id: 2, title: "Updated Title B" };
+
+  const action = courseActions.updateCoursesSuccess(courseUpdated);
+  // Act
+
+  const resultNewState = courseReducer(initialState, action);
+
+  // Assert
+
+  expect(resultNewState.length).toEqual(3);
+  expect(resultNewState[0].title).toEqual("A");
+  expect(resultNewState[1].title).toEqual("Updated Title B");
 });
